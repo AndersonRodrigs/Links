321b08d2429a3a4e2c835814784ec0b632364753
Anderson Rodrigues <105132452+AndersonRS080@users.noreply.github.com>
2022-12-12 15:06:30 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+ScrollReveal({
+  origin: 'left',
+  distance: '100px',
+  duration: 1500,
+  
+}).reveal(`.link , footer p, .image-header-content`, { interval: 100 })
+
+ScrollReveal({
+  origin: 'top',
+  distance: '100px',
+  duration: 1500,
+  
+}).reveal(`.info`, { interval: 100 })
